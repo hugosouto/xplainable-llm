a5438f9727dd9864d8664a7a54f7a9a93bfaebf2
Hugo Souto <hugomsouto@gmail.com>
2025-03-19 00:13:23 -0300

    Add initial implementation of sentence generation using OpenAI API

---
+++ main.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+client = OpenAI()
+
+word = "beer"
+n_words = 15
+n_sentences = 10
+sentences = []
+
+for i in range(n_sentences):
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": f"Complete a short sentence, wwith approximately {n_words} words long."},
+            {"role": "user", "content": f"The {word}"}
+        ],
+        response_format={"type": "text"},
+        temperature=0.05,
+        max_completion_tokens=50,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        store=True
+    )
+    sentences.append(completion.choices[0].message.content)
+
+# print(completion.choices[0].message.content)
+for sentence in sentences:
+    print(sentence)
